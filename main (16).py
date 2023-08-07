06416475f9da991489f47589dc88d198078ab658
Sanjay M R <sanjayilaya2002@gmail.com>
2023-08-07 19:43:03 +0530

    uploading my day 3 exercise1/ Odd or Even

---
+++ main (16).py
@@ -0,0 +1,12 @@
+
+number = int(input("Which number do you want to check? "))
+
+
+#Write your code below this line 
+remainder = number % 2
+if remainder != 0:
+    print("This is an odd number.")
+else:
+    print("This is an even number.")
+
+
